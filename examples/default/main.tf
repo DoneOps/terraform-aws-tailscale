0a2bb485a430afa07abe1662aaa7c8c767186c7e
Jonathan Meyers <jmymy@users.noreply.github.com>
2024-03-12 21:20:03 +0000

    chore: fix examples

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,7 +1,7 @@
 module "bastion_host" {
-  source               = "../../"
-  name                 = "foo"
-  vpc_id               = data.aws_vpc.default.id
-  subnet_id            = data.aws_subnets.default.ids[0]
-  advertised_routes    = data.aws_vpc.default.cidr_block
+  source            = "../../"
+  name              = "foo"
+  vpc_id            = data.aws_vpc.default.id
+  subnet_id         = data.aws_subnets.default.ids[0]
+  advertised_routes = data.aws_vpc.default.cidr_block
 }
--- examples/default/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instance_eip" {
-  value = module.bastion_host.instance_eip.public_ip
-}
